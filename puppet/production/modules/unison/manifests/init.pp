673bd32506fee745da6cf6e6ea3b014e8352bc2a
Stéphane Thomas <email@stephanethomas.com>
2018-09-11 15:22:03 +0200

    Fix wrong unison version downloaded
    The Arch Linux unison package was recently updated to version 2.51. This change makes sure we still download version 2.48. As an aside, version 2.51 was compiled with version 2.28 of the libc6 package which is only available on the future Cosmic Ubuntu release. That's why I chose to keep the current unison version - updating to the newest would indeed requires too much effort for now.

--- puppet/production/modules/unison/manifests/init.pp
+++ puppet/production/modules/unison/manifests/init.pp
@@ -1,10 +1,10 @@
 class unison {
   exec { "download":
-    command => "/usr/bin/wget --content-disposition https://www.archlinux.org/packages/extra/x86_64/unison/download/ -O /tmp/unison-2.48.3-2-x86_64.pkg.tar.xz"
+    command => "/usr/bin/wget --content-disposition https://archive.archlinux.org/packages/u/unison/unison-2.48.15-3-x86_64.pkg.tar.xz --directory-prefix=/tmp"
   }
-  
+
   exec { "uncompress":
-    command => "/bin/tar xf /tmp/unison-2.48.3-2-x86_64.pkg.tar.xz -C /",
+    command => "/bin/tar xf /tmp/unison-2.48.15-3-x86_64.pkg.tar.xz -C /",
     require => Exec["download"]
   }
 }
