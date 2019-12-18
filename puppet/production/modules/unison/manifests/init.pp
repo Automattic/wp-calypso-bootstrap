7ffe8d761cbc2394cf4df4c20903f0028ace94f3
Stéphane Thomas <email@stephanethomas.com>
2019-12-18 17:23:27 +0100

    Use more descriptive and unique names for tasks

--- puppet/production/modules/unison/manifests/init.pp
+++ puppet/production/modules/unison/manifests/init.pp
@@ -1,10 +1,10 @@
 class unison {
-  exec { "download":
+  exec { "download unison":
     command => "/usr/bin/wget --content-disposition https://archive.archlinux.org/packages/u/unison/unison-2.48.15-3-x86_64.pkg.tar.xz --directory-prefix=/tmp"
   }
 
-  exec { "uncompress":
+  exec { "uncompress unison":
     command => "/bin/tar xf /tmp/unison-2.48.15-3-x86_64.pkg.tar.xz -C /",
-    require => Exec["download"]
+    require => Exec["download unison"]
   }
 }
