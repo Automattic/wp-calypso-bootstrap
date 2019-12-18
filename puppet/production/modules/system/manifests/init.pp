ef85d5862c4720a61d5fa251781394f838dd0c25
Stéphane Thomas <email@stephanethomas.com>
2019-12-18 17:24:13 +0100

    Upgrade Node to version 12

--- puppet/production/modules/system/manifests/init.pp
+++ puppet/production/modules/system/manifests/init.pp
@@ -6,24 +6,18 @@ class system {
       id => "9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280",
       server => "hkp://keyserver.ubuntu.com:80"
     },
-    location => "https://deb.nodesource.com/node_10.x"
+    location => "https://deb.nodesource.com/node_12.x"
   }
 
-  exec { "update":
+  exec { "update system":
     command => "/usr/bin/apt-get update",
     require => Apt::Source["nodejs"]
   }
 
-
-  exec { "install keyboard configuration":
-    command => "/usr/bin/apt-get install --assume-yes keyboard-configuration",
-    require => Exec["update"]
-  }
-
-  exec { "upgrade":
+  exec { "upgrade system":
     command => "/usr/bin/apt-get upgrade --assume-yes",
     path    => ["/bin/", "/sbin/", "/usr/bin/", "/usr/sbin/"],
-    require => Exec["install keyboard configuration"],
+    require => Exec["update system"],
     timeout => 0
   }
 
